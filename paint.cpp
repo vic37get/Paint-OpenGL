8ada30c78808bba4cc233b2910530dd0d30f733a
Victor <victorribeiro172@gmail.com>
2023-06-24 13:21:05 -0300

    circunferencia e inicio flood fill

--- paint.cpp
+++ paint.cpp
@@ -27,8 +27,9 @@ using namespace std;
 // Variaveis Globais
 #define ESC 27
 
+int raio;
 //Enumeracao com os tipos de formas geometricas
-enum tipo_forma{LIN = 1, TRI, RET, POL, CIR}; // Linha, Triangulo, Retangulo Poligono, Circulo
+enum tipo_forma{LIN = 1, TRI, RET, POL, CIR, FLOOD}; // Linha, Triangulo, Retangulo Poligono, Circulo
 
 //Verifica se foi realizado o primeiro clique do mouse
 bool click1 = false, click2 = false, click3 = false;
@@ -44,6 +45,12 @@ int modo = LIN;
 
 //Largura e altura da janela
 int width = 1024, height = 768;
+
+typedef struct{
+	float r;
+	float b;
+	float g;
+}cor;
 
 // Definicao de vertice
 struct vertice{
@@ -92,10 +99,21 @@ void pushTriangulo(int x1, int y1, int x2, int y2, int x3, int y3){
 	
 }
 
-void pushQuadrado(int x1, int y1, int x2, int y2){
+void pushQuadrilatero(int x1, int y1, int x2, int y2){
 	pushForma(RET);
 	pushVertice(x1,y1);
 	pushVertice(x2,y2);
+}
+
+void pushCircunferencia(int x1, int y1, int x2, int y2, int origemX, int origemY){
+	pushForma(CIR);
+	pushVertice(x1, y1);
+	pushVertice(x2, y2);
+}
+
+void pushFloodFill(int x1, int y1){
+	pushForma(FLOOD);
+	pushVertice(x1,y1);
 }
 
 /*
@@ -115,11 +133,18 @@ void drawFormas();
 // Funcao que implementa o Algoritmo de Bresenham para rasterizacao de segmentos de retas.
 void linha(int x1, int y1, int x2, int y2);
 
+//Funcao que implementa o Quadrilatero
+void quadrilatero(int x1, int y1, int x2, int y2);
+
 //Funcao que implementa o Triangulo
 void triangulo(int x1, int y1, int x2, int y2, int x3, int y3);
 
-//Funcao que implementa o Quadrado
-void quadrado(int x1, int y1, int x2, int y2);
+//Funcao que implementa a Circunferencia
+void circunferencia(int x1, int x2, int origemX, int origemY, int raio);
+
+//Funcao que implementa a coloração FloodFill
+void floodFill(float x1, float y1, cor corAnterior, cor novaCor);
+void floodFillSR(int x1, int y1, cor corAnterior, cor novaCor);
 
 
 /*
@@ -227,7 +252,10 @@ void keyboard(unsigned char key, int x, int y){
     	case 99:
         	modo = CIR;
         	break;
-    	
+        //Tecla F, para o FloodFill.
+        case 102:
+        	modo = FLOOD;
+        	break;
         case ESC: exit(EXIT_SUCCESS); 
 			break;
     }
@@ -262,7 +290,7 @@ void mouse(int button, int state, int x, int y){
                             x_2 = x;
                             y_2 = height - y - 1;
                             printf("Clique 2(%d, %d)\n",x_2,y_2);
-                            pushQuadrado(x_1, y_1, x_2, y_2);
+                            pushQuadrilatero(x_1, y_1, x_2, y_2);
                             click1 = false;
                             glutPostRedisplay();
                         }else{
@@ -280,7 +308,7 @@ void mouse(int button, int state, int x, int y){
                             y_3 = height - y - 1;
                             click3 = true;
                             printf("Clique 3(%d, %d)\n",x_3,y_3);
-                            pushTriangulo(x_1, y_2, x_2, y_2, x_3, y_3);
+                            pushTriangulo(x_1, y_1, x_2, y_2, x_3, y_3);
                             click2 = false;
                             glutPostRedisplay();
                         }else if(click1){
@@ -299,27 +327,38 @@ void mouse(int button, int state, int x, int y){
 						}
                     }
 				}            	
-            	if (modo == POL){
+            	else if (modo == POL){
             		printf("POL!");
 				}
-            	if (modo == CIR){
-            		printf("PEGOU!");
+            	else if (modo == CIR){
+            		if (state == GLUT_DOWN) {
+                        if(click1){
+                            x_2 = x;
+                            y_2 = height - y - 1;
+                            printf("Clique 2(%d, %d)\n",x_2,y_2);
+                            pushCircunferencia(x_1, y_1, x_2, y_2, x_1, y_1);
+                            click1 = false;
+                            glutPostRedisplay();
+                        }else{
+                            click1 = true;
+                            x_1 = x;
+                            y_1 = height - y - 1;
+                            printf("Clique 1(%d, %d)\n",x_1,y_1);
+                        }
+                    }
 				}
-            }
-
-//        case GLUT_MIDDLE_BUTTON:
-//            if (state == GLUT_DOWN) {
-//                glutPostRedisplay();
-//            }
-//        break;
-//
-//        case GLUT_RIGHT_BUTTON:
-//            if (state == GLUT_DOWN) {
-//                glutPostRedisplay();
-//            }
-//        break;
-            
-    }
+				else if (modo == FLOOD){
+            		if (state == GLUT_DOWN) {
+            			click1 = true;
+						x_1 = x;
+                 	    y_1 = height - y - 1;
+				        printf("Clique 1(%d, %d)\n",x_1,y_1);
+						pushFloodFill(x_1, y_1);
+			            glutPostRedisplay();
+                    }
+				}
+	}
+}
 
 /*
  * Controle da posicao do cursor do mouse
@@ -334,6 +373,7 @@ void mousePassiveMotion(int x, int y){
  */
 void drawPixel(int x, int y){
     glBegin(GL_POINTS); // Seleciona a primitiva GL_POINTS para desenhar
+    	glColor3f(0.0f, 0.0f, 0.0f);
         glVertex2i(x, y);
     glEnd();  // indica o fim do ponto
 }
@@ -344,10 +384,14 @@ void drawPixel(int x, int y){
 void drawFormas(){
 	int i = 0;
     //Apos o primeiro clique, desenha a reta com a posicao atual do mouse
-
+	//Desenha o segmento de reta apos dois cliques
+	if (click1 && (modo == LIN || modo == RET) ){
+	   	linha(x_1, y_1, m_x, m_y);
+	}
     //Percorre a lista de formas geometricas para desenhar
     for(forward_list<forma>::iterator f = formas.begin(); f != formas.end(); f++){
         switch (f->tipo) {
+        	//Feita pegando dois pontos, após o segundo clique é desenhada a linha.
             case LIN:
             	i = 0;
 				int x[2];
@@ -357,14 +401,23 @@ void drawFormas(){
                     x[i] = v->x;
                     y[i] = v->y;
                 }
-                //Desenha o segmento de reta apos dois cliques
-                if (click1 && modo == LIN){
-			  		linha(x_1, y_1, m_x, m_y);
-				}
                 linha(x[0], y[0], x[1], y[1]);
                 break;
             
-            case TRI:
+            //Feito pegando dois pontos diagonais, após o segundo clique, é desenhado o quadrilátero.
+            case RET:
+      			i = 0;
+	   	   	   	int xq[2];
+				int yq[2];
+	            for(forward_list<vertice>::iterator v = f->v.begin(); v != f->v.end(); v++, i++){
+					xq[i] = v->x;
+					yq[i] = v->y;
+ 	 	        }
+	            quadrilatero(xq[0], yq[0], xq[1], yq[1]);
+	            break;
+	        
+	        //Feita pegando três pontos, após o terceiro clique é desenhado o triângulo.
+    		case TRI:
             	i = 0;
 				int xt[3];
 				int yt[3];
@@ -374,16 +427,38 @@ void drawFormas(){
                 }
                 triangulo(xt[0], yt[0], xt[1], yt[1], xt[2], yt[2]);
                 break;
-           case RET:
-      			i = 0;
-	   	   	   	int xq[2];
-				int yq[2];
-	            for(forward_list<vertice>::iterator v = f->v.begin(); v != f->v.end(); v++, i++){
-				        xq[i] = v->x;
-				        yq[i] = v->y;
- 	 	        }
-	            quadrado(xq[0], yq[0], xq[1], yq[1]);
-	            break;
+            
+            //Feita pegando dois pontos como o raio, após o segundo clique é desenhada a circunferência.
+            case CIR:
+            	i = 0;
+				int xc[2];
+				int yc[2];
+				int origemX, origemY;
+				
+                for(forward_list<vertice>::iterator v = f->v.begin(); v != f->v.end(); v++, i++){
+                    xc[i] = v->x;
+                    yc[i] = v->y;
+                }
+                raio = (std::sqrt(std::pow((xc[1] - xc[0]), 2) + std::pow((yc[1] - yc[0]), 2)))/2;
+                origemX = (xc[1] + xc[0])/2;
+                origemY = (yc[1] + yc[0])/2;
+                circunferencia(xc[0], yc[0], origemX, origemY, raio);
+                break;
+            
+            case FLOOD:
+            	i = 0;
+				int xf[2];
+				int yf[2];
+				
+                for(forward_list<vertice>::iterator v = f->v.begin(); v != f->v.end(); v++, i++){
+                    xf[i] = v->x;
+                    yf[i] = v->y;
+                }
+                cor corAnterior = {1.0f, 1.0f, 1.0f};
+                cor novaCor = {1.0f, 0.0f, 0.0f};
+                floodFillSR(xf[0], yf[0], corAnterior, novaCor);
+                break;
+           	 			            
         }
     }
 }
@@ -470,20 +545,98 @@ void linha(int x1, int y1, int x2, int y2){
 	drawPixel(pontoX2, pontoY2);
 }
 	
-//----------------------------------------- DESENHO DE TRIÂNGULOS ----------------------------------------------------------//
-
-// Função que implementa os triângulos.
-void triangulo(int x1,int y1, int x2, int y2, int x3, int y3){
-	linha(x1, y1, x2, y2);
-	linha(x2, y2, x3, y3);
-	linha(x1, y1, x3, y3);
-}
-
-
 //----------------------------------------- DESENHO DE QUADRILATEROS ----------------------------------------------------------//
-void quadrado(int x1, int y1, int x2, int y2){
+void quadrilatero(int x1, int y1, int x2, int y2){
 	linha(x1, y1, x2,y1);
 	linha(x2, y1, x2, y2);
 	linha(x2, y2, x1,y2);
 	linha(x1, y2, x1, y1);
+}
+
+//----------------------------------------- DESENHO DE TRIÂNGULOS ----------------------------------------------------------//
+void triangulo(int x1,int y1, int x2, int y2, int x3, int y3){
+	linha(x1, y1, x2, y2);
+	linha(x2, y2, x3, y3);
+	linha(x3, y3, x1, y1);
+}
+
+//----------------------------------------- DESENHO DE CIRCUNFERÊNCIA ----------------------------------------------------------//
+void espelhamento(int x1, int y1, int origemX, int origemY){
+	//Primeiro quadrante.
+	drawPixel(x1+origemX, y1+origemY);
+	drawPixel(y1+origemX, x1+origemY);
+	
+	//Segundo quadrante.
+	drawPixel(-x1+origemX, y1+origemY);
+	drawPixel(y1+origemX, -x1+origemY);
+	
+	//Terceiro quadrnte.
+	drawPixel(-x1+origemX, -y1+origemY);
+	drawPixel(-y1+origemX, -x1+origemY);
+	
+	//Quarto quadrante.
+	drawPixel(x1+origemX, -y1+origemY);
+	drawPixel(-y1+origemX, x1+origemY);
+}
+
+void circunferencia(int x1, int y1, int origemX, int origemY, int raio){
+	int d, deltaE, deltaSE, xi, yi;
+	d = 1 - raio;
+	deltaE = 3;
+	deltaSE = -(2*raio)+5;
+	xi = 0, yi = raio;
+	while(yi > xi){
+		xi = xi + 1;
+		if (d < 0){
+			d += deltaE;
+			deltaE += 2;
+			deltaSE += 2;
+		}
+		else{
+			d += deltaSE;
+			deltaE += 2;
+			deltaSE += 4;
+			yi--;
+		}
+		espelhamento(xi, yi, origemX, origemY);
+	}
+
+}
+
+//----------------------------------------- FLOOD FILL ----------------------------------------------------------//
+void floodFill(float x1, float y1, cor corAnterior, cor novaCor){
+	cor corPixel;
+	glReadPixels(x1, y1, 1, 1, GL_RGB, GL_FLOAT, &corPixel);
+	// printf("Cor anterior: (%f, %f, %f)\n", corAnterior.r, corAnterior.g, corAnterior.b);
+	// printf("Nova cor: (%f, %f, %f)\n", novaCor.r, novaCor.g, novaCor.b);
+	// printf("Cor lida: (%f, %f, %f)\n", corPixel.r, corPixel.g, corPixel.b);
+	if (corPixel.r == corAnterior.r && corPixel.g == corAnterior.g && corPixel.b == corAnterior.b){
+		glBegin(GL_POINTS);
+			glColor3f(novaCor.r, novaCor.g, novaCor.b);
+			glVertex2i(x1, y1);
+		glEnd();
+		glFlush();	
+		floodFill(x1+1, y1, corAnterior, novaCor);
+		floodFill(x1, y1+1, corAnterior, novaCor);
+		floodFill(x1-1, y1, corAnterior, novaCor);
+		floodFill(x1, y1-1, corAnterior, novaCor);
+	}
+	
+}
+
+void floodFillSR(int x1, int y1, cor corAnterior, cor novaCor){
+	cor corPixel;
+	bool naoColorido = true;
+	while (naoColorido == true){
+		glReadPixels(x1, y1, 1, 1, GL_RGB, GL_FLOAT, &corPixel);
+		if(corPixel.r == 0.0f && corPixel.g == 0.0f && corPixel.b == 0.0f){
+			printf("Chegou ao pixel preto");
+			break;
+		}
+		x1++;
+			// printf("Cor lida: (%f, %f, %f)\n", corPixel.r, corPixel.g, corPixel.b);
+			// printf("Cor anterior: (%f, %f, %f)\n", corAnterior.r, corAnterior.g, corAnterior.b);
+			// printf("Nova cor: (%f, %f, %f)\n", novaCor.r, novaCor.g, novaCor.b);	
+	}
+	
 }
